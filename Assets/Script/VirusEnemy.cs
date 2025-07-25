a41049be51fc777adafc830dbd544dc08c0bd65e
masa101224 <masa101224@outlook.jp>
2025-07-25 16:23:44 +0900

    vineenemyのコード修正

--- Assets/Script/VirusEnemy.cs
+++ Assets/Script/VirusEnemy.cs
@@ -4,13 +4,65 @@ using UnityEngine;
 
 public class VirusEnemy : EnemyBase
 {
+    public float moveDistance = 3f;
+    private Vector2 startPoint;
+    private int moveDirection = 1; //êiçsï˚å¸ÇÃêßå‰
+    public float detection = 5f;
+    private GameObject player;
+    private bool isChasing = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        startPoint = transform.position;
+    }
+
     protected override void Move()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player)
+        if (player == null)
+        {
+             player = GameObject.FindGameObjectWithTag("Player");
+            if (player == null)
+            {
+                return;
+            }
+
+        }
+
+        float distancePlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distancePlayer < detection)
+        {
+            isChasing = true;
+        }
+        else if (distancePlayer > detection * 1.2f)
+        {
+            isChasing = false;
+        }
+
+        if (isChasing)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrole();
+        }
+    }
+    private void ChasePlayer()
         {
             Vector2 dir = (player.transform.position - transform.position).normalized;
             transform.Translate(dir * moveSpeed * 2f * Time.deltaTime);
         }
+
+    private void Patrole()
+    {
+        transform.Translate(Vector2.right * moveDirection * moveSpeed * Time.deltaTime);
+
+        if (Mathf.Abs(transform.position.x - startPoint.x) > moveDistance)
+        {
+            moveDirection *= -1; // ï˚å¸îΩì]
+        }
     }
+
 }
